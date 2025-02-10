12485f09b004357e390d6f46ef5f8916ec8b3d49
yoonjonglee <33422963+yoonjonglee@users.noreply.github.com>
2025-02-10 18:55:43 +0900

    Update MaxDivisorAndMinMultiplyNumbers01.py

--- AlgorithmLectureForCompleteBeginnersInProgramming/MaxDivisorAndMinMultiplyNumbers01.py
+++ AlgorithmLectureForCompleteBeginnersInProgramming/MaxDivisorAndMinMultiplyNumbers01.py
@@ -1,34 +1,31 @@
 import sys
 
 """
-10 4
-
-1 2 5
-1 2 4
-
 """
 
 print("input any number")
 input = sys.stdin.readline
 num1, num2 = input().split()
 #num = int(input())
+num1 = int(num1)
+num2 = int(num2)
 
 if num1 >= num2:
-    numBig = int(num1)
-    numSmall = int(num2)
+    numBig = num1
+    numSmall = num2
 else:
-    numBig = int(num2)
-    numSmall = int(num1)
-    
-
-    
+    numBig = num2
+    numSmall = num1
     
 dlists = []
         
-for x in range(1, num+1):
-    if num % x == 0:
-        dlists.append(x)
+for i in range (1, numSmall+1):
+    if numBig % i == 0 and numSmall % i == 0:
+        print(f"{i} is a devisor")
+        dlists.append(i)
         
 print(dlists)
-print(f"summation of the results is {sum(dlists)}")
-print(f"numbers of the results is {len(dlists)}")
+GCD = max(dlists)
+SCM = max(dlists) * numBig/max(dlists) * numSmall/max(dlists)
+print(f"greatest common devisor is {GCD}")
+print(f"smalleast common multipy is {SCM}")
