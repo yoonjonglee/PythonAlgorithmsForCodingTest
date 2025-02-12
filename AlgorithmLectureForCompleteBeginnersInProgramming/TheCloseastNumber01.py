6ac7be4bd6b53a1612e045eed6ba098be735127b
yoonjonglee <33422963+yoonjonglee@users.noreply.github.com>
2025-02-12 12:29:13 +0900

    Create TheCloseastNumber01.py

---
+++ AlgorithmLectureForCompleteBeginnersInProgramming/TheCloseastNumber01.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline()
+"""
+"""
+print("input any numbers")
+nList = list(map(int, input().split()))
+print("input the number to be closed")
+num = int(input())
+
+gapList = []
+
+for i in range(len(nList)):
+     gapList.append(abs(nList[i] - num))
+
+print(f"the closeast number with 3 is {nList[gapList.index(min(gapList))]}")
