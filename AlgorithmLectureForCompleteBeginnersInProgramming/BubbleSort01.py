171abf1f77c57622386a2ff1cabbea8c7701bf1c
yoonjonglee <33422963+yoonjonglee@users.noreply.github.com>
2025-02-13 20:34:57 +0900

    Create BubbleSort01.py

---
+++ AlgorithmLectureForCompleteBeginnersInProgramming/BubbleSort01.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+"""
+4231
+42(01)->2431
+43(12)->2341
+41(23)->2314
+23(01)->2314
+31(12)->2134
+21(01)->1234
+"""
+print("input any integer numbers to be bubble sorted")
+#nList = list(map(int, input().split()))
+#result = sorted(nList, reverse = True)
+
+for i in range():
+    #0 1 2 3
+    for i+1 in range():
+        #1 2 3 4
+        
