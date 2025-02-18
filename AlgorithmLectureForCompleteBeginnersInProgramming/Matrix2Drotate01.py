ca0b815ba1756e4af5c5deb5d7f13a58cf496109
yoonjonglee <33422963+yoonjonglee@users.noreply.github.com>
2025-02-18 20:14:01 +0900

    Create Matrix2Drotate01.py

---
+++ AlgorithmLectureForCompleteBeginnersInProgramming/Matrix2Drotate01.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+"""
+"""
+#input
+print("input any integer numbers")
+num = int(input())
+print("input any integer numbers")
+nList2Din = [list(map(int, input().split())) for _ in range(num)]
+#result = sorted(nList, reverse = True)
+
+#process
+
+#output
+print(nList2Din)
