a33bcf6e17496514fcc31d5521702c91cb7d54ca
yoonjonglee <33422963+yoonjonglee@users.noreply.github.com>
2025-02-21 15:08:30 +0900

    Create Table9X9List01.py

---
+++ AlgorithmLectureForCompleteBeginnersInProgramming/Table9X9List01.py
@@ -0,0 +1,23 @@
+import sys
+input = sys.stdin.readline
+
+"""
+"""
+#input
+#print("input any integer numbers")
+#num = int(input())
+#print("input any integer numbers")
+#nList2Din = [list(map(int, input().split())) for _ in range(num)]
+#result = sorted(nList, reverse = True)
+
+#process
+#nList2dOut = [[0]*num_col for _ in range(num_row)]
+nListOut = []
+
+for i in range(2, 10):
+    for j in range(1, 10):
+        print(f"{i}X{j}={i*j}")
+        nListOut.append(i*j)
+
+#output
+print(nListOut)
