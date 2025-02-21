fe38bf4e3627b4e0cd4ca5e30391d9eb04e4228e
yoonjonglee <33422963+yoonjonglee@users.noreply.github.com>
2025-02-21 15:29:27 +0900

    Update Table9X9List01.py

--- AlgorithmLectureForCompleteBeginnersInProgramming/Table9X9List01.py
+++ AlgorithmLectureForCompleteBeginnersInProgramming/Table9X9List01.py
@@ -4,8 +4,8 @@ input = sys.stdin.readline
 """
 """
 #input
-#print("input any integer numbers")
-#num = int(input())
+print("input any integer numbers")
+num = int(input())
 #print("input any integer numbers")
 #nList2Din = [list(map(int, input().split())) for _ in range(num)]
 #result = sorted(nList, reverse = True)
@@ -14,10 +14,15 @@ input = sys.stdin.readline
 #nList2dOut = [[0]*num_col for _ in range(num_row)]
 nListOut = []
 
-for i in range(2, 10):
-    for j in range(1, 10):
-        print(f"{i}X{j}={i*j}")
-        nListOut.append(i*j)
+if num != 0:
+    for i in range(1, 10):
+        print(f"{num}X{i}={num*i}")
+        nListOut.append(num*i)
+else:
+     for i in range(2, 10):
+         for j in range(1, 10):
+             print(f"{i}X{j}={i*j}")
+             nListOut.append(i*j)
 
 #output
 print(nListOut)
